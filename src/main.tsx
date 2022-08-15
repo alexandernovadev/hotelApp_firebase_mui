ece5a8ad666a9d80f87f71e0f5cad6dd785c05a4
alexander nova <alexsk88.dev@gmail.com>
2022-08-15 14:30:53 -0500

    {ADD} Translate ok

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import "./i18n"
+
+// import es_locale from "./locales/en.json"
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
